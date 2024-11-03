e1198b3dd121724968f2a7a4de94aeb4f5d8a862
mtgrinstead <michael@mgrinstead.com>
2024-11-03 10:35:14 -0500

    fixed formatting

--- json.go
+++ json.go
@@ -27,7 +27,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-    if _, err := w.Write(dat); err != nil {
-        log.Printf("failed to write response: %v", err)
-    }
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("failed to write response: %v", err)
+	}
 }
